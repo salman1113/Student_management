cfc141b99461d9ab90a7583aa4a9a444540413f6
salman <mrsalmanxzs@gmail.com>
2025-11-17 16:38:19 +0530

    repush

--- student_mgmt_project/settings.py
+++ student_mgmt_project/settings.py
@@ -143,5 +143,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "mrsalmanxzs@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "bywk dfqk sjkm tfqq"
 
